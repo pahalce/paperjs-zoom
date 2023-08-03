2e9176c7a3d7bb82a5b48292518d1dadb581125a
pahalce <peaceee.programming@gmail.com>
2023-08-03 23:45:43 +0900

    test

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,29 +2,25 @@ import paper from "paper";
 import { useEffect, useRef } from "react";
 import reactIcon from "../assets/react.svg";
 
-function initGraph() {
-  //Add Image
-  const raster = new paper.Raster(reactIcon);
-  raster.scale(10);
-  raster.opacity = 1;
-  raster.position = paper.view.center; //
+// function initGraph() {
+//   //Add Image
+//   const raster = new paper.Raster(reactIcon);
+//   raster.scale(10);
+//   raster.opacity = 1;
+//   raster.position = paper.view.center; //
 
-  //Add Points randomly sized and randomly colored
-  for (let i = 0; i < 100; i++) {
-    addRandomPoint();
-  }
-}
+//   //Add Points randomly sized and randomly colored
+//   for (let i = 0; i < 100; i++) {
+//     addRandomPoint();
+//   }
+// }
 
-window.onload = () => {
-  const canvas = document.querySelector("canvas");
-  if (canvas === null) {
-    return;
-  }
+window.onload = function () {
+  // Get a reference to the canvas object
+  const canvas = document.getElementById("my-canvas") as HTMLCanvasElement;
+  // Create an empty project and a view for the canvas:
   paper.setup(canvas);
-  initGraph();
-};
 
-function addRandomPoint() {
   const radius = 2.0 * Math.random() + 2.0;
 
   const randomFillColor =
@@ -39,11 +35,32 @@ function addRandomPoint() {
   const y = Math.random() * paper.view.size.height;
 
   new paper.Path.Circle({
-    center: new paper.Point(x, y),
-    radius: radius,
+    center: new paper.Point(100, 100),
+    radius: 100,
     fillColor: randomFillColor,
   });
-}
+};
+
+// function addRandomPoint() {
+//   const radius = 2.0 * Math.random() + 2.0;
+
+//   const randomFillColor =
+//     "rgb(" +
+//     ~~(256 * Math.random()) +
+//     ", " +
+//     ~~(256 * Math.random()) +
+//     ", " +
+//     ~~(256 * Math.random()) +
+//     ")";
+//   const x = Math.random() * paper.view.size.width;
+//   const y = Math.random() * paper.view.size.height;
+
+//   new paper.Path.Circle({
+//     center: new paper.Point(x, y),
+//     radius: radius,
+//     fillColor: randomFillColor,
+//   });
+// }
 
 const Canvas = () => {
   const canvasRef = useRef(null);
@@ -85,6 +102,7 @@ const Canvas = () => {
 
   return (
     <canvas
+      id="my-canvas"
       ref={canvasRef}
       className="w-full bg-blue-200 border border-black"
     ></canvas>
